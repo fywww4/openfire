252ab1b50e49a696f5a3be0f7d13a5ce1962612b
Bread <a0900518266@gmail.com>
2025-09-24 14:50:25 +0800

    上傳美術圖跟背景滾動程式

---
+++ Assets/Resource/Scripts/BG_move.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_move : MonoBehaviour
+{
+
+    public float speed = 1f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+        if (transform.position.y < -10)
+            transform.position = new Vector3(0, 10, 0);
+    }
+}
