fa9c72febfdb460bba0d1b16085b8389924b518b
Bread <a0900518266@gmail.com>
2025-10-08 14:35:37 +0800

    上傳爆炸特效 子彈三連發

--- Assets/Resource/Scripts/Player_control.cs
+++ Assets/Resource/Scripts/Player_control.cs
@@ -6,15 +6,15 @@ public class Player_control : MonoBehaviour
 {
     public float speed = 2.0f;
     public GameObject projectile;
- 
+
     // Update is called once per frame
     void Update()
     {
-        
+
         transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, speed * Input.GetAxis("Vertical") * Time.deltaTime, 0);
 
         if (transform.position.x < -3.25f) //偵測是否超出左邊邊界座標x=-3.25
-            transform.position= new Vector3(-3.25f,transform.position.y, transform.position.z);//重新設定物件的Y軸座標在-3.25的位置
+            transform.position = new Vector3(-3.25f, transform.position.y, transform.position.z);//重新設定物件的Y軸座標在-3.25的位置
 
         if (transform.position.x > 3.25f) //偵測是否超出右邊邊界座標x=-3.25
             transform.position = new Vector3(3.25f, transform.position.y, transform.position.z);//重新設定物件的Y軸座標在3.25的位置
@@ -27,6 +27,10 @@ public class Player_control : MonoBehaviour
 
 
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             Instantiate(projectile, transform.position, transform.rotation);
+            Instantiate(projectile, transform.position + new Vector3(0.5f, -0.5f, 0f), transform.rotation );
+            Instantiate(projectile, transform.position + new Vector3(-0.5f, -0.5f, 0f), transform.rotation );
+        }
     }
 }
---
+++ Assets/Resource/Scripts/Rock_move.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Rock_move : MonoBehaviour
+{
+    public float speed = 2.5f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0); //讓物件以Speed值,向下移動
+
+        if (transform.position.y < -6f)//判斷物件是否超出遊戲畫面外,若是,則執行底下的程式
+        {
+            transform.position = new Vector3(Random.Range(-2.8f, 2.8f), 8f, -0.5f); //把物件放到一個新的座標,X為-2.82.8之間
+            speed = Random.Range(2f, 4f); //賦予物件新的速度(從2~4取一隨機值)
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            transform.position = new Vector3(Random.Range(-2.8f, 2.8f), 6f, -0.5f);
+            speed = Random.Range(2f, 4f);
+            Destroy(other.gameObject);
+        }
+        if(other.tag == "Player")
+        {
+            transform.position = new Vector3(Random.Range(-2.8f, 2.8f), 6f, -0.5f);
+        }
+    }       
+}
