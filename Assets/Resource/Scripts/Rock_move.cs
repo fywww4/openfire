4fefdbda2b5a44632e66701662550c5d877316df
Bread <a0900518266@gmail.com>
2025-10-15 14:40:38 +0800

    石頭爆炸、玩家爆炸、倒數計時、生命值、分數、背景音樂、音效

--- Assets/Resource/Scripts/Rock_move.cs
+++ Assets/Resource/Scripts/Rock_move.cs
@@ -8,6 +8,8 @@ public class Rock_move : MonoBehaviour
     public float speed = 2.5f;
     public GameObject bombrock;
     public GameObject bombplayer;
+    public static int life = 3;
+    public static int score = 0;
 
     // Update is called once per frame
     void Update()
@@ -24,13 +26,22 @@ public class Rock_move : MonoBehaviour
     {
         if (other.tag == "Finish")
         {
+            Instantiate(bombrock, transform.position, transform.rotation);
             transform.position = new Vector3(Random.Range(-2.8f, 2.8f), 6f, -0.5f);
             speed = Random.Range(2f, 4f);
             Destroy(other.gameObject);
+            score += 10;
         }
         if(other.tag == "Player")
         {
+            Instantiate(bombplayer, other.transform.position, other.transform.rotation);
             transform.position = new Vector3(Random.Range(-2.8f, 2.8f), 6f, -0.5f);
+            life--;
         }
-    }       
+    } 
+    void OnGUI()
+    {
+        GUI.Label(new Rect(130, 40, 120, 180),score.ToString());
+        GUI.Label(new Rect(470, 40, 60, 180),life.ToString());
+    }
 }
---
+++ Assets/Resource/Scripts/TimeRemain.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeRemain : MonoBehaviour
+{
+    public Texture[] timeNumbers;
+    public static int leftTime = 100;
+    float myTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTime += Time.deltaTime;
+        if (myTime > 1)
+        {
+            leftTime--;
+            myTime = 0;
+        }
+    }
+
+    void OnGUI()
+    {
+        for (int i = 0; i < leftTime.ToString().Length; i++)
+            GUI.DrawTexture(new Rect(265 + i * 32, 20, 32, 45), timeNumbers[System.Int32.Parse((leftTime.ToString())[i].ToString())]);
+    }
+}
